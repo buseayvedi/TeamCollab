d5427294800d04351a37a083dc8ded61ce479c75
melikecorasag <melikecora12@gmail.com>
2024-01-26 09:00:42 -0600

    Updated FromOneHundred class

--- src/interview_codes/arrays/FromOneTwoHundred.java
+++ src/interview_codes/arrays/FromOneTwoHundred.java
@@ -4,7 +4,14 @@ public class FromOneTwoHundred {
 
     public static void main(String[] args) {
 
+        int one = "A".length();
+        int hundred = "AAAAAAAAAA".length();
+        hundred *= hundred;
 
+        while (hundred>=one){
+            System.out.print(one + " ");
+            one++;
+        }
 
 
     }
