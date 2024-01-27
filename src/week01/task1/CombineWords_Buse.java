d9a0344243728c184dc95527c7f810e5f419f3dc
European01 <emiryaziciofficial@gmail.com>
2024-01-26 23:13:56 -0500

    Update the whole project.

---
+++ src/week01/task1/CombineWords_Buse.java
@@ -0,0 +1,15 @@
+package week01.task1;
+
+public class CombineWords_Buse {
+    /*
+    Create a method that can combine two Strings then print the combined version.
+    But if the last letter of the first String and the first letter of the second String is the same, print that character once.
+
+    Example;
+                Input:
+                   String1= one
+                   String2 = eight
+                Output:
+                     oneight
+     */
+}
