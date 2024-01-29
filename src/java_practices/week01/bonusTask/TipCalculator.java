a07b6dbd90351e964a83aaf53deb5addf4721c0e
buseayvedi <buseayvedi@gmail.com>
2024-01-29 00:16:27 -0500

    add new method to TipCalculator

--- src/java_practices/week01/bonusTask/TipCalculator.java
+++ src/java_practices/week01/bonusTask/TipCalculator.java
@@ -1,5 +1,7 @@
 package java_practices.week01.bonusTask;
 
+import java.util.Scanner;
+
 public class TipCalculator {
 /*
     Write a program for a tip calculator.
@@ -38,4 +40,55 @@ public class TipCalculator {
                 Total per person: 148.75
                 Tip per person: 29.75
  */
+public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    tipCalculatorBuse(new Scanner(System.in));
+
+}
+    public static void tipCalculatorBuse(Scanner scan){
+        System.out.println("Split or No split (Yes or No)");
+        String split = scan.next();
+
+        System.out.println("Enter the number of people (number)");
+        int numberOfPeople = scan.nextInt();
+
+        System.out.println("Enter the check amount (number)");
+        int checkAmount = scan.nextInt();
+
+        System.out.println("Service Quality (Poor, Fair, Good, Great, Excellent");
+        String quality = scan.next();
+        scan.close();
+
+        double totalTip = 0;
+
+        if (quality.equalsIgnoreCase("Poor")) {
+            totalTip = checkAmount * 0.05;
+        } else if (quality.equalsIgnoreCase("Fair")) {
+            totalTip = checkAmount * 0.1;
+        } else if (quality.equalsIgnoreCase("Good")) {
+            totalTip = checkAmount * 0.15;
+        } else if (quality.equalsIgnoreCase("Great")) {
+            totalTip = checkAmount * 0.2;
+        } else if (quality.equalsIgnoreCase("Excellent")) {
+            totalTip = checkAmount * 0.25;
+        } else {
+            System.out.println("Invalid quality");
+        }
+
+        double totalPay = totalTip + checkAmount;
+        double totalPerPerson = totalPay / numberOfPeople;
+        double tipPerPerson = totalTip / numberOfPeople;
+
+        if (split.equalsIgnoreCase("Yes")) {
+
+            System.out.println("Number of people entered " + numberOfPeople + "\nTotal to pay: $" + totalPay + "\n Total tip: $" + totalTip + "\nTotal per person: $" + totalPerPerson + "\nTip per person: $" + tipPerPerson);
+
+        } else if (split.equalsIgnoreCase("No")) {
+            System.out.println("Number of people entered " + numberOfPeople + "\nTotal to pay: $" + totalPay + "\nTotal tip: $" + totalTip);
+
+        } else {
+            System.out.println("Invalid answer");
+        }
+
+    }
 }
