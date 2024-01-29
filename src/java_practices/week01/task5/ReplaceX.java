8e9f38a323a4786d64f5d50bfd7a58824b29b002
melikecorasag <melikecora12@gmail.com>
2024-01-28 21:51:11 -0600

    Update ReplaceX class

--- src/java_practices/week01/task5/ReplaceX.java
+++ src/java_practices/week01/task5/ReplaceX.java
@@ -10,4 +10,23 @@ Example;
                     Output:
                         acodea
  */
+
+    public static void main(String[] args) {
+
+        System.out.println(replaceAllXMelike("xcodeX"));
+        System.out.println(replaceAllXMelike("Dxllxs"));
+
+
+    }
+
+    public static String replaceAllXMelike(String str){
+
+        str = str.replaceAll("x","a");
+        str = str.replaceAll("X","a");
+        return str;
+
+    }
+
+
+
 }
